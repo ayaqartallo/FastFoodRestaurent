a726c5a451fcf0846fba7abdb349263b29f6875b
unknown <aya.q2000@gmail.com>
2023-09-08 16:44:39 +0300

    project files + responsive

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,32 +1,42 @@
-import React from "react";
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from 'react-router-dom'
 import "./navbar.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faCartShopping, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 export const Navbar = () => {
-    return <div className="navbar">
-        <div className="logo">
-            <h1>Fast Food</h1>
-        </div>
-        <div className="links">
-            <Link to="/"> HOME </Link>
-            <Link to="/menu"> MENU </Link>
-            <Link to="/about"> ABOUT </Link>
-            <Link to="/booktable"> BOOK TABLE </Link>
-        </div>
-        <div className="icon-button-container">
-            <div className="icons">
-                <Link to="/contact">
-                <FontAwesomeIcon icon={faUser} />
-                </Link>
-                <Link to="/cart">
-                <FontAwesomeIcon icon={faCartShopping} />
-                </Link>
-                <Link to="/search">
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-                </Link>
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <div className="navbar">
+            <div className="logo">
+                <h1>Fast Food</h1>
             </div>
-            <button>Order Online</button>
+            <div className="mobile-menu" onClick={() => setMenuOpen(!menuOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div className={menuOpen ? "links open" : "links"}>
+                <Link to="/"> HOME </Link>
+                <Link to="/menu"> MENU </Link>
+                <Link to="/about"> ABOUT </Link>
+                <Link to="/booktable"> BOOK TABLE </Link>
+            </div>
+            <div className="icon-button-container">
+                <div className="icons">
+                    <Link to="/contact">
+                        <FontAwesomeIcon icon={faUser} />
+                    </Link>
+                    <Link to="/cart">
+                        <FontAwesomeIcon icon={faCartShopping} />
+                    </Link>
+                    <Link to="/search">
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </Link>
+                </div>
+                <button>Order Online</button>
+            </div>
         </div>
-    </div>;
+    );
 };
